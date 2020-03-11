ebf9252a0bfb51998e9c950a797945f4009a4551
Burtenshaw <ben.burtenshaw@gmail.com>
2020-03-11 08:55:57 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='SeeWhence',
+      version='0.0.1',
+      description='This package is for sequence classification.',
+      author='Ben Burtenshaw',
+      author_email='ben.burtenshaw@gmail.com',
+#       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+#       license='LICENSE.txt',
+    )
